25910a670719446e1b46363032804b4dee7e2080
Sergei Efanov <s-s-efanov@yandex-team.ru>
2024-09-19 20:55:47 +0300

    Initial Commit

---
+++ TCADemo/TCADemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADemoApp.swift
+//  TCADemo
+//
+//  Created by Sergei Efanov on 19.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TCADemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCADemoTests/TCADemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  TCADemoTests.swift
+//  TCADemoTests
+//
+//  Created by Sergei Efanov on 19.09.2024.
+//
+
+import Testing
+
+struct TCADemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
